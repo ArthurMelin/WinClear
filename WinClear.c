35642ba9d56519f8fa5ebdd43afdd5d4e07b9eb4
Elassyo <elassyo@users.noreply.github.com>
2016-08-05 15:48:27 +0200

    Code formatting + Added some checks

--- WinClear.c
+++ WinClear.c
@@ -35,51 +35,56 @@ typedef struct __attribute__((__packed__)) ACPI_SDT_HEADER
 	UINT32 CreatorRevision;
 } ACPI_SDT_HEADER;
 
-typedef struct __attribute__((__packed__)) ACPI_BGRT {
-    ACPI_SDT_HEADER Header;
-    UINT16 Version;
-    UINT8 Status;
-    UINT8 ImageType;
-    UINT64 ImageAddress;
-    UINT32 ImageOffsetX;
-    UINT32 ImageOffsetY;
+typedef struct __attribute__((__packed__)) ACPI_BGRT
+{
+	ACPI_SDT_HEADER Header;
+	UINT16 Version;
+	UINT8 Status;
+	UINT8 ImageType;
+	UINT64 ImageAddress;
+	UINT32 ImageOffsetX;
+	UINT32 ImageOffsetY;
 } ACPI_BGRT;
 
-typedef struct __attribute__((__packed__)) BMP_HEADER {
-    CHAR8 Signature[2];
-    UINT32 Size;
-    CHAR8 Reserved[4];
-    UINT32 ImageDataOffset;
+typedef struct __attribute__((__packed__)) BMP_HEADER
+{
+	CHAR8 Signature[2];
+	UINT32 Size;
+	CHAR8 Reserved[4];
+	UINT32 ImageDataOffset;
 } BMP_HEADER;
 
 static BOOLEAN Cleared = FALSE;
 
-static BOOLEAN parse_acpi_sdt_entry(UINT64 address) {
-    ACPI_SDT_HEADER * acpi_sdt_header = (ACPI_SDT_HEADER *)address;
+static BOOLEAN parse_acpi_sdt_entry(UINT64 address)
+{
+	ACPI_SDT_HEADER * acpi_sdt_header = (ACPI_SDT_HEADER *)address;
 
-    if (CompareMem(acpi_sdt_header->Signature, "BGRT", 4)) return FALSE;
-    PrintDebug(L"Found BGRT address: 0x%X%X\n\n", address>>32, address);
+	if (CompareMem(acpi_sdt_header->Signature, "BGRT", 4)) return FALSE;
+	PrintDebug(L"Found BGRT address: 0x%X%X\n\n", address>>32, address);
 
-    ACPI_BGRT * acpi_bgrt = (ACPI_BGRT *)address;
-    if (acpi_bgrt->Version != 1 || acpi_bgrt->ImageType != 0 || !acpi_bgrt->ImageAddress) {
-        Print(L"Unsupported BGRT revision or image type\n");
-        return FALSE;
-    }
+	ACPI_BGRT * acpi_bgrt = (ACPI_BGRT *)address;
+	if (acpi_bgrt->Header.Length != sizeof(ACPI_BGRT) || acpi_bgrt->Header.Revision != 1 || acpi_bgrt->Version != 1 || acpi_bgrt->ImageType != 0 || !acpi_bgrt->ImageAddress)
+	{
+		Print(L"Invalid or unsupported BGRT or boot image type\n");
+		return FALSE;
+	}
 
-    PrintDebug(L"Found boot logo bitmap address: 0x%X%X\n", acpi_bgrt->ImageAddress>>32, acpi_bgrt->ImageAddress);
+	PrintDebug(L"Found boot logo bitmap address: 0x%X%X\n", acpi_bgrt->ImageAddress>>32, acpi_bgrt->ImageAddress);
 
-    BMP_HEADER * bmp_header = (BMP_HEADER *)acpi_bgrt->ImageAddress;
-    if (CompareMem(bmp_header->Signature, "BM", 2)) {
-        Print(L"Invalid bitmap header signature: %.2a\n", bmp_header->Signature);
-        return FALSE;
-    }
+	BMP_HEADER * bmp_header = (BMP_HEADER *)acpi_bgrt->ImageAddress;
+	if (CompareMem(bmp_header->Signature, "BM", 2))
+	{
+		Print(L"Invalid bitmap header signature: %.2a\n", bmp_header->Signature);
+		return FALSE;
+	}
 
-    PrintDebug(L"Clearing bitmap data (Size: %d bytes)\n", bmp_header->Size);
-    ZeroMem(bmp_header, bmp_header->Size);
-    Cleared = TRUE;
-    PrintDebug(L"Boot logo cleared!\n\n");
+	PrintDebug(L"Clearing bitmap data (Size: %d bytes)\n", bmp_header->Size);
+	ZeroMem(bmp_header, bmp_header->Size);
+	Cleared = TRUE;
+	PrintDebug(L"Boot logo cleared!\n\n");
 
-    return TRUE;
+	return TRUE;
 }
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
@@ -102,39 +107,41 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 
 			if (acpi_rsdp->Revision == 1)
 			{
-			    UINT64 RsdtAddress = acpi_rsdp->RsdtAddress;
-			    UINT64 RsdtEntriesAddress = RsdtAddress + 36;
+				UINT64 RsdtAddress = acpi_rsdp->RsdtAddress;
+				UINT64 RsdtEntriesAddress = RsdtAddress + 36;
 				ACPI_SDT_HEADER * acpi_rsdt_header = (ACPI_SDT_HEADER *)RsdtAddress;
 				UINT32 * acpi_rsdt_entries = (UINT32 *)RsdtEntriesAddress;
 				UINTN acpi_rsdt_number_of_entries = (acpi_rsdt_header->Length - 36) / sizeof(UINT32);
 				PrintDebug(L"%d entries in RSDT\n", acpi_rsdt_number_of_entries);
 				for (UINTN j = 0; j < acpi_rsdt_number_of_entries; j++)
 				{
-				    if (parse_acpi_sdt_entry((UINT64)acpi_rsdt_entries[j])) break;
+					if (parse_acpi_sdt_entry((UINT64)acpi_rsdt_entries[j])) break;
 				}
 			}
 			else if (acpi_rsdp->Revision == 2)
-            {
+			{
 				ACPI_SDT_HEADER * acpi_xsdt_header = (ACPI_SDT_HEADER *)acpi_rsdp->XsdtAddress;
 				UINT64 * acpi_xsdt_entries = (UINT64 *)(acpi_rsdp->XsdtAddress + 36);
 				UINTN acpi_xsdt_number_of_entries = (acpi_xsdt_header->Length - 36) / sizeof(UINT64);
 				PrintDebug(L"%d entries in XSDT\n", acpi_xsdt_number_of_entries);
 				for (UINTN j = 0; j < acpi_xsdt_number_of_entries; j++)
 				{
-				    if (parse_acpi_sdt_entry((UINT64)acpi_xsdt_entries[j])) break;
+					if (parse_acpi_sdt_entry((UINT64)acpi_xsdt_entries[j])) break;
 				}
 			}
-			else {
-                Print(L"ACPI RSDP table revision unsupported. Cannot proceed\n\n");
+			else
+			{
+				Print(L"ACPI RSDP table revision unsupported. Cannot proceed\n\n");
 			}
 
-            break;
+			break;
 		}
 	}
 
-	if (!Cleared) {
-        Print(L"Failed to clear boot logo\nStarting Windows Boot Manager in 3 seconds\n");
-        uefi_call_wrapper(BS->Stall, 1, 3*1000*1000);
+	if (!Cleared)
+	{
+		Print(L"Failed to clear boot logo\nStarting Windows Boot Manager in 3 seconds\n");
+		uefi_call_wrapper(BS->Stall, 1, 3*1000*1000);
 	}
 
 	EFI_STATUS Ret;
